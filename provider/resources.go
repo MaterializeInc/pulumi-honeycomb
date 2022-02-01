343a0b848776862b7508a6e2e73a028df81cca71
Andreas Fuchs <asf@materialize.com>
2022-02-01 12:17:03 -0500

    Don't provide explicit configuration settings for the provider
    They're gathered automatically from the terraform provider, and the settings we had
    here were _overrides_. Let's drop that and hopefully, pulumi won't
    complain about missing config settings that it already has.

--- provider/resources.go
+++ provider/resources.go
@@ -50,32 +50,16 @@ func Provider() tfbridge.ProviderInfo {
 
 	// Create a Pulumi provider mapping
 	prov := tfbridge.ProviderInfo{
-		P:                 p,
-		Name:              "honeycombio",
-		Description:       "A Pulumi package for creating and managing honeycomb.io resources.",
-		Keywords:          []string{"pulumi", "honeycomb", "tracing", "o11y", "monitoring", "otel"},
-		License:           "Apache-2.0",
-		GitHubOrg:         "honeycombio",
-		Homepage:          "https://github.com/honeycombio/terraform-provider-honeycombio",
-		Repository:        "https://github.com/honeycombio/terraform-provider-honeycombio",
-		PluginDownloadURL: fmt.Sprintf("https://github.com/MaterializeInc/pulumi-honeycomb/releases/download/v%s/", version.Version),
-		Config: map[string]*tfbridge.SchemaInfo{
-			"api_key": {
-				Default: &tfbridge.DefaultInfo{
-					EnvVars: []string{"HONEYCOMBIO_APIKEY"},
-				},
-			},
-			"api_url": {
-				Default: &tfbridge.DefaultInfo{
-					Value: "https://api.honeycomb.io",
-				},
-			},
-			"debug": {
-				Default: &tfbridge.DefaultInfo{
-					Value: false,
-				},
-			},
-		},
+		P:                    p,
+		Name:                 "honeycombio",
+		Description:          "A Pulumi package for creating and managing honeycomb.io resources.",
+		Keywords:             []string{"pulumi", "honeycomb", "tracing", "o11y", "monitoring", "otel"},
+		License:              "Apache-2.0",
+		GitHubOrg:            "honeycombio",
+		Homepage:             "https://github.com/honeycombio/terraform-provider-honeycombio",
+		Repository:           "https://github.com/honeycombio/terraform-provider-honeycombio",
+		PluginDownloadURL:    fmt.Sprintf("https://github.com/MaterializeInc/pulumi-honeycomb/releases/download/v%s/", version.Version),
+		Config:               map[string]*tfbridge.SchemaInfo{},
 		PreConfigureCallback: preConfigureCallback,
 		Resources: map[string]*tfbridge.ResourceInfo{
 			"honeycombio_board":            {Tok: tfbridge.MakeResource(mainPkg, mainMod, "Board")},
